fac6a6426bfd71eafeff2adae73a6e028d429826
Charlie Chen <charlie.dd.chen@gmail.com>
2025-10-26 18:59:48 -0400

    edited comments

--- alu32_server/source/sr32.sv
+++ alu32_server/source/sr32.sv
@@ -9,10 +9,6 @@ module sr32
     logic [31:0] shifted0, shifted1, shifted2, shifted3;
     logic fill; // fill bit: 0 if not arithmetic, otherwise MSB (in[31])
     and f (fill, arithmetic, in[31]); // gives us our fill bit so we can use it whenever
-        
-    // Note: I'm working on this too late to ask course staff if this adheres to the guidelines, but I'm doing it in a sort of roundabout way so that
-    // the logic here only affects how I assign output wires of one stage to input wires of the next stage -- if needed, I could manually write out each 2:1 mux
-    // input (that would just be tedious, and what this if statement aims to avoid); no extra gates get created by this.
     
     // if we will need to shift by 1
     right_stage1 shifter0 (
